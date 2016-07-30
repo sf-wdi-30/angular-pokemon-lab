8b0e06a0d541cfe70494cf091733f5176a4e76fb
SwittS <switt.srikulacheep@gmail.com>
2016-07-30 16:31:33 -0700

    revert back to list because of table issue

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,58 @@
 console.log('PokeIndexController succesfully online!');
 
 angular
-  .module('pokemon', [])
-  .controller('PokeIndexController', PokeIndexController);
+    .module('pokemon', [])
+    .controller('PokeIndexController', PokeIndexController);
 
 PokeIndexController.$inject = ['$http'];
 
-  function PokeIndexController ($http) {
+function PokeIndexController($http) {
     var app = this;
     app.newPoke = {};
 
-  $http({
-      method: 'GET',
-      url: 'https://super-crud.herokuapp.com/pokemon'
+    $http({
+        method: 'GET',
+        url: 'https://super-crud.herokuapp.com/pokemon'
     }).then(function successCallback(response) {
-      app.poke = response.data.pokemon;
-      console.log(app.poke);
-
+        app.poke = response.data.pokemon;
+        console.log(app.poke);
     }, function errorCallback(response) {
-      console.log('There was an error getting the data', response);
+        console.log('There was an error getting the data', response);
     });
 
-    app.createPoke = function () {
+    app.createPoke = function() {
 
-      $http({
-        method: 'POST',
-        url: 'https://super-crud.herokuapp.com/pokemon',
-        data: app.newPoke,
-      }).then(function successCallback(response) {
-        app.pokemon.push(response.data);
-      }, function errorCallback(response) {
-        console.log('There was an error posting the data', response);
-      });
+        $http({
+            method: 'POST',
+            url: 'https://super-crud.herokuapp.com/pokemon',
+            data: app.newPoke,
+        }).then(function successCallback(response) {
+            app.pokemon.push(response.data);
+        }, function errorCallback(response) {
+            console.log('There was an error posting the data', response);
+        });
     };
 
-    app.deletePoke = function (pokemon) {
+    app.deletePoke = function(pokemon) {
 
-    $http({
-      method: 'DELETE',
-      url: 'https://super-crud.herokuapp.com/pokemon/' + pokemon._id
-    }).then(function successCallback(json) {
-      var index = app.poke.indexOf(pokemon);
-      app.poke.splice(index,1);
-    }, function errorCallback(response) {
-      console.log('There was an error deleting the data', response);
-    });
-  };
-  }
+        $http({
+            method: 'DELETE',
+            url: 'https://super-crud.herokuapp.com/pokemon/' + pokemon._id
+        }).then(function successCallback(json) {
+            var index = app.poke.indexOf(pokemon);
+            app.poke.splice(index, 1);
+        }, function errorCallback(response) {
+            console.log('There was an error deleting the data', response);
+        });
+    };
+
+    app.editPoke = function(pokemon) {
+        $http({
+            method: 'PUT',
+            url: 'https://super-crud.herokuapp.com/pokemon/' + pokemon._id
+        }).then(function successCallback(json) {
+        }, function errorCallback(response) {
+            console.log('There was an error editing the data', response);
+        });
+    };
+}
