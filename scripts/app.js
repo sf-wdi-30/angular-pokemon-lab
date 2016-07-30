0bafc9633be9f8835cd12e39731ba52c093f3fed
mehgellan <baumegg@gmail.com>
2016-07-30 11:39:29 -0700

    add showPoke function

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,38 @@
 console.log('app.js loaded');
 
+// INITIALIZE ANGULAR APP
 var app = angular.module('pokemonApp', []);
+// INITIALIZE ANGULAR CONTROLLER
 app.controller('PokemonIndexController', PokemonIndexController);
-
+// INJECT $HTTP MODULE
 PokemonIndexController.$inject = ['$http'];
 
+// CONTROLLER FUNCTION
 function PokemonIndexController($http) {
-  this.newPoke = {};
+  var me = this;
+  // me.newPoke = {};
 
-  
+  $http({
+    method:'GET',
+    url: 'https://super-crud.herokuapp.com/pokemon'
+  }).then(function successCb(response) {
+    me.pokemon = response.data.pokemon; // me.pokemon === []
+    // console.log(me.pokemon);
+  }, function errorCb(response) {
+    console.log('ERROR', response);
+  });
+
+  me.showPoke = function(poke) {
+    $http({
+      method: 'GET',
+      url: 'https://super-crud.herokuapp.com/pokemon/' + poke._id
+    }).then(function successCb(response) {
+      me.poke = response.data;
+      console.log(me.poke);
+    }, function errorCb(response) {
+      console.log('ERROR', response);
+    });
+  };
+
+
 }
