3f0033d56289f2bd56b66058429ca696f0ff0bb7
SwittS <switt.srikulacheep@gmail.com>
2016-07-30 12:55:28 -0700

    able to render list to page

--- scripts/app.js
+++ scripts/app.js
@@ -4,15 +4,19 @@ angular
   .module('pokemon', [])
   .controller('PokeIndexController', PokeIndexController);
 
-  function PokeIndexController () {
+PokeIndexController.$inject = ['$http'];
+
+  function PokeIndexController ($http) {
     var app = this;
     app.newPoke = {};
 
   $http({
       method: 'GET',
-      url: '/super-crud.herokuapp.com/pokemon'
+      url: 'https://super-crud.herokuapp.com/pokemon'
     }).then(function successCallback(response) {
-      app.pokemon = response.data;
+      app.pokemon = response.data.pokemon;
+      console.log(app.pokemon);
+
     }, function errorCallback(response) {
       console.log('There was an error getting the data', response);
     });
