47d5431cb5bf2e7d1cd44cbe123940532e55a9d7
SwittS <switt.srikulacheep@gmail.com>
2016-07-30 13:20:08 -0700

    begin work on create/delete

--- scripts/app.js
+++ scripts/app.js
@@ -14,11 +14,23 @@ PokeIndexController.$inject = ['$http'];
       method: 'GET',
       url: 'https://super-crud.herokuapp.com/pokemon'
     }).then(function successCallback(response) {
-      app.pokemon = response.data.pokemon;
-      console.log(app.pokemon);
+      app.poke = response.data.pokemon;
+      console.log(app.poke);
 
     }, function errorCallback(response) {
       console.log('There was an error getting the data', response);
     });
 
-}
+    app.createPoke = function () {
+
+      $http({
+        method: 'POST',
+        url: 'https://super-crud.herokuapp.com/pokemon',
+        data: app.newPoke,
+      }).then(function successCallback(response) {
+        app.pokemon.push(response.data);
+      }, function errorCallback(response) {
+        console.log('There was an error posting the data', response);
+      });
+    };
+  }
